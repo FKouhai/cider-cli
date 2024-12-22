d064976057dd84e874c7613e6fed98af3853528f
FKouhai <frandres00@gmail.com>
2024-12-22 22:35:24 +0100

    fatalling instead of println

--- methods/client.go
+++ methods/client.go
@@ -13,17 +13,17 @@ func Connect(endpoint string, token string, method string) ([]byte, error){
 		//resp, err := client.Post()
 	}
 		req, err := http.NewRequest("GET", endpoint, nil); if err != nil {
-			log.Println(err)
+			log.Fatalln(err)
 			return nil,err
 		}
 		req.Header.Add("apptoken", token)
 		resp, err := client.Do(req); if err != nil {
-			log.Println(err)
+			log.Fatalln(err)
 			return nil,err
 		}
 		defer resp.Body.Close()
 		body, err = io.ReadAll(resp.Body); if err != nil {
-			log.Println(err)
+			log.Fatalln(err)
 			return nil,err
 		}
 		return body,nil
